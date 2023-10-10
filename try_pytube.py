385feb444c245d2e046f4690024b89bc8f3e8391
firiusz <firiusz123@gmail.com>
2023-10-10 04:16:21 +0200

    the download update
    well still cannot pick the folder for the download tho

--- try_pytube.py
+++ try_pytube.py
@@ -2,9 +2,10 @@ from pytube import YouTube
 
 
 yt = YouTube("https://youtu.be/OjNpRbNdR7E?feature=shared")
-streams= yt.streams.filter(file_extension='mp4', progressive=True)
+streams= yt.streams.filter(file_extension='mp4', progressive=True , res="720p")
 resolution=[]
-for stream in streams:
-    resolution.append(stream.resolution)
+tag = streams[0].itag
+print(tag)
+stream = yt.streams.get_by_itag(tag)
 
-print(resolution)
+
